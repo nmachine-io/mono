9892c0d10bd3f85391443902ce272fc48043d78d
xavier <xavier@codenectar.com>
2020-04-03 15:07:20 +0200

    more doodling

--- moz/storage_concern.py
+++ moz/storage_concern.py
@@ -1,6 +1,7 @@
 from k8_kat.res.pvc.kat_pvc import KatPvc
 from moz.mosaic_concern import MosaicConcern
 from wiz.model.part import Part
+from wiz.model.step import Step
 
 
 class SetPvcNamePart(Part):
@@ -8,8 +9,16 @@ class SetPvcNamePart(Part):
   def list_pvcs(self):
     KatPvc.q()
 
+class SetPvcNameStep(Step):
+
+  def commit(self):
+    pass
 
 class StorageConcern(MosaicConcern):
+
+  @classmethod
+  def step(cls, name) -> Step:
+    pass
 
   def __init__(self):
     pass
