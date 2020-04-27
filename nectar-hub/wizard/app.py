12d3e1613e1728c0167669db779fde3b576d6369
xavier <xavier@codenectar.com>
2020-04-27 15:44:05 +0200

    docker build/push working

--- nectar-hub/wizard/app.py
+++ nectar-hub/wizard/app.py
@@ -5,7 +5,8 @@ from wiz import server
 from wiz.core.wiz_globals import wiz_globals
 
 def load_yaml_array(fname) -> [Dict]:
-  return yaml.load(open(fname, 'r').read())['data']
+  file_contents = open(fname, 'r').read()
+  return yaml.load(file_contents, Loader=yaml.FullLoader)['data']
 
 wiz_globals.set_configs(
   concerns=load_yaml_array('wizard/constants/concerns.yaml'),
