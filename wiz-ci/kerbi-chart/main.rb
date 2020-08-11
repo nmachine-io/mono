1328425976891556743aff91206a67e293d3bab3
xavier <xavier@codenectar.com>
2020-08-11 10:21:17 +0200

    wiz-ci using kerbi's new cli parser

--- wiz-ci/kerbi-chart/main.rb
+++ wiz-ci/kerbi-chart/main.rb
@@ -3,12 +3,9 @@ require 'kerbi'
 class SimpleMixer < Kerbi::Mixer
   locate_self __dir__
   def run
-    super do |res|
-      res.yaml 'res'
-    end
+    super { |res| res.yaml 'res' }
   end
 end
 
 kerbi.generators = [ SimpleMixer ]
-
-puts kerbi.gen_yaml
+kerbi.cli_exec
