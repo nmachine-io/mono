82e4081512a1fda2955ec07990282322a8db05f6
xavier <xavier@codenectar.com>
2020-08-25 22:55:48 +0200

    good stab

--- flaky/kerbi/main.rb
+++ flaky/kerbi/main.rb
@@ -1,3 +1,14 @@
-class MainMixer < Kerbi::Mixer
+require 'kerbi'
 
+class MainMixer < Kerbi::Mixer
+  locate_self __dir__
+  def run
+    super do |g|
+      g.yaml 'dep_and_svc'
+    end
+  end
 end
+
+kerbi.generators = [ MainMixer ]
+
+puts kerbi.cli_exec
--- self-hosted-nectar-hub/wizard/templaters/helm/go.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker build . -t interp
-kubectl delete pod -l app=interp -n moz
-kubectl apply -f pod.yaml
