db3e74b0f2beb591d1038af52f15ae428cc8a584
xavier <xavier@codenectar.com>
2020-08-26 15:52:05 +0200

    moving towards flake wiz

--- flake/wiz/main.py
+++ flake/wiz/main.py
@@ -1,25 +1,18 @@
 import dotenv
 
-from sample_wiz.adapters import app_endpoints
-from sample_wiz.overrides import fields, steps
+from overrides import app_endpoints
 from wiz import server
 from wiz.core import utils
 from wiz.core.wiz_app import wiz_app
 
-def prep():
-  op_yamls_base = 'sample_wiz/operation-yamls'
+op_yamls_base = 'sample_wiz/operation-yamls'
 
-  wiz_app.add_configs(
-    utils.yamls_in_dir(f'{op_yamls_base}/installation') +
-    utils.yamls_in_dir(f'{op_yamls_base}/move-to-own-pvc') +
-    utils.yamls_in_dir(f'{op_yamls_base}/easy-win') +
-    utils.yamls_in_dir(f'sample_wiz/variables-yamls')
-  )
+wiz_app.add_configs(utils.yamls_in_dir('configs'))
+wiz_app.add_providers([app_endpoints.AppEndpointsProvider])
 
-  wiz_app.add_providers([
-    app_endpoints.AppEndpointsProvider
-  ])
+wiz_app.app_name = 'flake'
+wiz_app.tami_name = 'gcr.io/nectar-bazaar/flake-tami:latest'
+wiz_app.tami_args = '-e hub-self-hosted'
 
-  wiz_app.app_name = 'flake'
-  wiz_app.tami_name = 'gcr.io/nectar-bazaar/nectar-tedi:latest'
-  wiz_app.tami_args = '-e hub-self-hosted'
+dotenv.load_dotenv()
+server.start()
