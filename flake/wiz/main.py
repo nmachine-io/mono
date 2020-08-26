b39e9af2ed1acb77efec9f1c5d4c472b0c87b7f1
xavier <xavier@codenectar.com>
2020-08-26 09:17:54 +0200

    simple kubectl apply flake working

---
+++ flake/wiz/main.py
@@ -0,0 +1,25 @@
+import dotenv
+
+from sample_wiz.adapters import app_endpoints
+from sample_wiz.overrides import fields, steps
+from wiz import server
+from wiz.core import utils
+from wiz.core.wiz_app import wiz_app
+
+def prep():
+  op_yamls_base = 'sample_wiz/operation-yamls'
+
+  wiz_app.add_configs(
+    utils.yamls_in_dir(f'{op_yamls_base}/installation') +
+    utils.yamls_in_dir(f'{op_yamls_base}/move-to-own-pvc') +
+    utils.yamls_in_dir(f'{op_yamls_base}/easy-win') +
+    utils.yamls_in_dir(f'sample_wiz/variables-yamls')
+  )
+
+  wiz_app.add_providers([
+    app_endpoints.AppEndpointsProvider
+  ])
+
+  wiz_app.app_name = 'flake'
+  wiz_app.tami_name = 'gcr.io/nectar-bazaar/nectar-tedi:latest'
+  wiz_app.tami_args = '-e hub-self-hosted'
