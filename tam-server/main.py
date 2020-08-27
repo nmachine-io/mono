4b2d392da7774b6ff79246175f9b4448bdbef740
xavier <xavier@codenectar.com>
2020-08-28 00:26:41 +0200

    worked when main imported

--- tam-server/main.py
+++ tam-server/main.py
@@ -7,19 +7,16 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
-executable = os.environ.get('TAMI_CMD', f'/home/xavier/workspace/nectarines/tam-server/insane.sh')
+executable = os.environ.get('TAMI_CMD', f'{os.getcwd()}/insane.sh')
 
 @app.route('/values')
 def values():
+  print(executable)
   split_cmd = f"{executable} values".split(" ")
-  yaml_out = subprocess.check_output(split_cmd).decode('utf-8')
+  yaml_out = subprocess.check_output(split_cmd, stderr=subprocess.STDOUT).decode('utf-8')
   as_dict = yaml.load(yaml_out)
   return jsonify(data=as_dict)
 
 
 app.config["cmd"] = ["bash"]
 app.run(host='0.0.0.0', port=5005, debug=True)
-
-
-
-
