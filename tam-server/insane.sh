cd95bbff489bf2f806cff2d881349a426beba46e
xavier <xavier@codenectar.com>
2020-08-27 23:42:11 +0200

    values working tam-server

---
+++ tam-server/insane.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/xavier/workspace/nectarines/wiz-ci/kerbi
+bundle exec ruby main.rb "$@"
---
+++ tam-server/main.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+
+import yaml
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
+executable = os.environ.get('TAMI_CMD', f'/home/xavier/workspace/nectarines/tam-server/insane.sh')
+
+@app.route('/values')
+def values():
+  split_cmd = f"{executable} values".split(" ")
+  yaml_out = subprocess.check_output(split_cmd).decode('utf-8')
+  as_dict = yaml.load(yaml_out)
+  return jsonify(data=as_dict)
+
+
+app.config["cmd"] = ["bash"]
+app.run(host='0.0.0.0', port=5005, debug=True)
+
+
+
+
