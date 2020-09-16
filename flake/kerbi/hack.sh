ddbe6b62c6e33e1533aca6509d6cddb3c07c62de
xavier <xavier@codenectar.com>
2020-09-15 22:04:00 -0400

    mainly nectwiz updates

--- flake/kerbi/hack.sh
+++ flake/kerbi/hack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-pub_image="gcr.io/nectar-bazaar/flake-tami:0.0.1"
+pub_image="gcr.io/nectar-bazaar/flake-tami:latest"
 docker build . -t flake-wiz -t $pub_image
 docker push $pub_image
--- flake/the_wiz/hack.sh
+++ flake/the_wiz/hack.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-nectwiz_version=$(awk -F== '{print $2}' <<< $(pipenv graph | grep nectwiz))
+#pipenv update
+#nectwiz_version=$(awk -F== '{print $2}' <<< $(pipenv graph | grep nectwiz))
+nectwiz_version='latest'
 pub_image="gcr.io/nectar-bazaar/flake-wiz:$nectwiz_version"
 docker build . -t flake-wiz -t $pub_image
 docker push $pub_image
