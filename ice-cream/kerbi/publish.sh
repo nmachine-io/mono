db54156cbea348ce236ade27f9cafbac5d03ee63
xavier <xavier@codenectar.com>
2020-09-17 09:34:11 -0400

    hack for 1.0/2.0 kerbi working

--- ice-cream/kerbi/publish.sh
+++ ice-cream/kerbi/publish.sh
@@ -5,5 +5,5 @@ tag=$1
 local_image="ice-cream-tami"
 public_image="gcr.io/nectar-bazaar/$local_image"
 
-docker build . -t $local_image:$tag -t $public_image:$tag
-docker push $public_image
+docker build . -t $local_image:$tag -t $public_image:$tag --build-arg VERSION=$tag
+docker push $public_image:$tag
