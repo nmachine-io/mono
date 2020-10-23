00a202aa4c7fa3484ce209e88a039ab49863c30f
xavier <xavier@codenectar.com>
2020-10-23 16:45:52 +0200

    minor fixes

--- telem-tami/publish.sh
+++ telem-tami/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-tag=$1
+tag="$1"
 
 local_image="telem-tami"
 public_image="gcr.io/nectar-bazaar/$local_image"
 
-docker build . -t $local_image:$tag -t $public_image:$tag
-docker push $public_image:$tag
+docker build . -t "$local_image:$tag" -t "$public_image:$tag"
+docker push "$public_image:$tag"
