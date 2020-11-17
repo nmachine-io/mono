242db60255c084d4ba48a5bbdc278194336ee546
xavier <xavier@codenectar.com>
2020-11-17 22:48:01 +0100

    helm Dockerfile working

---
+++ ice-cream/helm/dev-eval.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# shellcheck disable=SC2164
+cd ~/workspace/nectarines/ice-cream/helm
+helm "$@"
--- ice-cream/kerbi/dev-eval.sh
+++ ice-cream/kerbi/dev-eval.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# shellcheck disable=SC2164
 cd ~/workspace/nectarines/ice-cream/kerbi
-bundle exec ruby main.rb $@
+bundle exec ruby main.rb "$@"
