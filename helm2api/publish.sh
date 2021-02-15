3390dbf669ba47a706d2fe285423a80bd5c42256
xavier <xavier@codenectar.com>
2021-02-15 14:03:12 +0000

    publish.sh -> cloudbuild.yaml

--- helm2api/publish.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-tag="$1"
-local_image="helm2api"
-public_image="gcr.io/nectar-bazaar/$local_image"
-docker build . -t "$local_image:$tag" -t "$public_image:$tag"
-docker push "$public_image:$tag"
