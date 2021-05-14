a32b94aaedd8f2a24161aeb1175d08bfe2be8a3f
xavier <xavier@codenectar.com>
2021-05-14 15:28:41 +0100

    presets with v34

--- ice-cream/kama/main.py
+++ ice-cream/kama/main.py
@@ -2,13 +2,23 @@ import os
 
 from kama_sdk import entrypoint
 from kama_sdk.model.base.model import models_man
-from kama_sdk.core.core import utils
+from kama_sdk.core.core import utils, kaml_man
+from prom_kaml.kaml import PromKaml
+from telem_kaml.kaml import TelemKaml
 
-if __name__ == '__main__':
+
+def register_self():
   root_dir = os.path.dirname(os.path.abspath(__file__))
   yamls = utils.yamls_in_dir(f'{root_dir}/configs', recursive=True)
-
   models_man.add_descriptors(yamls)
 
-  entrypoint.start()
 
+def register_libraries():
+  kaml_man.register_kaml(TelemKaml)
+  kaml_man.register_kaml(PromKaml)
+
+
+if __name__ == '__main__':
+  register_libraries()
+  register_self()
+  entrypoint.start()
