390fc399b0de7dbbc24860dfe0327652382ef3bd
xavier <xavier@codenectar.com>
2021-05-21 14:06:25 +0100

    weird stupid release_name confusion gone

--- ice-cream/kama/main.py
+++ ice-cream/kama/main.py
@@ -3,19 +3,17 @@ import os
 from kama_sdk import entrypoint
 from kama_sdk.model.base.model import models_man
 from kama_sdk.core.core import utils, kaml_man
-from prom_kaml.kaml import PromKaml
-from telem_kaml.kaml import TelemKaml
 
 
 def register_self():
   root_dir = os.path.dirname(os.path.abspath(__file__))
   yamls = utils.yamls_in_dir(f'{root_dir}/configs', recursive=True)
-  models_man.add_descriptors(yamls)
+  models_man.add_app_descriptors(yamls)
 
 
 def register_libraries():
-  kaml_man.register_kaml(TelemKaml)
-  kaml_man.register_kaml(PromKaml)
+  kaml_man.register_kaml('telem_kaml')
+  kaml_man.register_kaml('prom_kaml')
 
 
 if __name__ == '__main__':
