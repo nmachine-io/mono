726a9718a594ca7991f8f7486ada7c7637d5582c
xavier <xavier@codenectar.com>
2021-08-01 19:41:35 +0100

    ice kream store is poppin

--- platform-kama/main.py
+++ platform-kama/main.py
@@ -8,9 +8,8 @@ from kama_sdk.core.core import utils, kaml_man
 def register_self():
   root_dir = os.path.dirname(os.path.abspath(__file__))
   yamls = utils.yamls_in_dir(f'{root_dir}/configs', recursive=True)
-  models_man.add_app_descriptors(yamls)
+  models_man.add_descriptors(yamls)
   models_man.add_asset_dir_paths([f'{root_dir}/assets'])
-
 
 def register_libraries():
   kaml_man.register_kaml('telem_kaml')
