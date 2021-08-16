06972713d6864efbcdc92c6630ffeb045d45bb0b
xavier <xavier@codenectar.com>
2021-08-16 11:23:50 +0100

    ruby-gcloud savin time

--- ice-kream/ice-kream-kama/main.py
+++ ice-kream/ice-kream-kama/main.py
@@ -1,22 +1,25 @@
 import os
 
 from kama_sdk.cli import entrypoint
-from kama_sdk.model.base.model import models_man
-from kama_sdk.utils import utils, kaml_man
+from kama_sdk.core.core import plugins_manager
+from kama_sdk.model.base.model import models_manager
+from kama_sdk.utils.descriptor_utils import load_dir_yamls
 
 
 def register_self():
   root_dir = os.path.dirname(os.path.abspath(__file__))
-  yamls = utils.yamls_in_dir(f'{root_dir}/configs', recursive=True)
-  models_man.add_descriptors(yamls)
-  models_man.add_asset_dir_paths([f'{root_dir}/assets'])
+  descriptors = load_dir_yamls(f'{root_dir}/descriptors')
 
-def register_libraries():
-  kaml_man.register_kaml('telem_kaml')
-  kaml_man.register_kaml('prom_kaml')
+  models_manager.add_descriptors(descriptors)
+  models_manager.add_asset_dir_paths([f'{root_dir}/assets'])
+
+
+def register_plugins():
+  plugins_manager.register('telem_kaml')
+  plugins_manager.register('prom_kaml')
 
 
 if __name__ == '__main__':
-  register_libraries()
+  # register_libraries()
   register_self()
   entrypoint.start()
